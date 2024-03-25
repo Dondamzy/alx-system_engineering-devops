d666635bc5298ba6fcfe2db36e3c07ea7001d808
Dondamzy <Dondamzy99@gmail.com>
2024-03-24 19:02:47 -0700

    using puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#using puppet to make changes to our config file
+
+file { 'etc/ssh/ssh_config':
+	ensure => present,
+
+content =>"
+
+	SSH client configuration
+	host*
+	IdentityFile ~/.ssh/school
+	PasswordAuthentification no
+
+}
